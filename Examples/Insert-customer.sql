d18a3c4270ced74c54cff77806184f1b18e05767
Kevin Pettinger <kevinpettinger@gmail.com>
2020-03-05 23:34:48 -0800

    output example

---
+++ Examples/Insert-customer.sql
@@ -0,0 +1,20 @@
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Kuschke", "Colly", "9532301985", "ckuschke0@ucsd.edu" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Hackin", "Gretal", "5978851627", "ghackin1@privacy.gov.au" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Sterman", "Aleta", "3427609158", "asterman2@yahoo.com" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Mourant", "Gerhardine", "4172293058", "gmourant3@icio.us" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Soffe", "Lane", "4781990943", "lsoffe4@ovh.net" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Singyard", "Renell", "1764034968", "rsingyard5@boston.com" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Yglesias", "Wandie", "9776503987", "wyglesias6@google.it" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Mcettrick", "Mervin", "1057405648", "mmcettrick7@quantcast.com" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Downton", "Reg", "7299862283", "rdownton8@barnesandnoble.com" );
+INSERT INTO customer( LastName, FirstName, Phone, Email )
+ VALUES("Relph", "Saw", "7309049621", "srelph9@domainmarket.com" );

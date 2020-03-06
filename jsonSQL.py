1d164640a3a0a73217ebeb2ad958e56d75ff55c5
Kevin Pettinger <kevinpettinger@gmail.com>
2020-03-05 23:15:28 -0800

    variable changes

--- jsonSQL.py
+++ jsonSQL.py
@@ -13,9 +13,9 @@ sqlScript = ""
 for row in table:
     sql = "INSERT INTO {}(".format(fileName)
     values = " VALUES("
-    for key in ob:
+    for key in row:
         sql = sql + " " + key + ","
-        values = values + '{}'.format(ob[key]) + ", "
+        values = values + '{}'.format(row[key]) + ", "
     sql = sql[0:len(sql)-1] + " )\n"
     values = values[0:len(values)-2] + " );\n"
     sqlScript = sqlScript + sql + values
